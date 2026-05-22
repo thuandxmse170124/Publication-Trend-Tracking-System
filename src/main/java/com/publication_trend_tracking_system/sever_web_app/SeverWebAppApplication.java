133e5571e66874e468475a868fe5b0284927d644
Thuan <thuandxmse170124@fpt.edu.vn>
2026-05-22 22:56:38 +0700

    Initial commit

---
+++ src/main/java/com/publication_trend_tracking_system/sever_web_app/SeverWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.publication_trend_tracking_system.sever_web_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeverWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeverWebAppApplication.class, args);
+	}
+
+}
