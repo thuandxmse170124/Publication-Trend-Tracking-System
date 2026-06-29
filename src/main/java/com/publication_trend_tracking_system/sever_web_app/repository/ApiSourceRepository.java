d65cf02bcf481142b35a62421e630fb904265b0a
BaoQuyyy <phanlebaohuy1989@gmail.com>
2026-06-29 19:00:18 +0700

    perf: fix findAll bombs in InvoiceServiceImpl

--- src/main/java/com/publication_trend_tracking_system/sever_web_app/repository/ApiSourceRepository.java
+++ src/main/java/com/publication_trend_tracking_system/sever_web_app/repository/ApiSourceRepository.java
@@ -4,4 +4,5 @@ import com.publication_trend_tracking_system.sever_web_app.entity.ApiSource;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ApiSourceRepository extends JpaRepository<ApiSource, Integer> {
+    java.util.Optional<ApiSource> findByStatusIgnoreCase(String status);
 }
